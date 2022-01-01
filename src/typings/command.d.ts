d657fb63862da624d847ff60d8efd73c784c9814
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-01 21:23:43 +0530

    🕷

---
+++ src/typings/command.d.ts
@@ -0,0 +1,40 @@
+import MessageHandler from '../Handlers/MessageHandler'
+import { WAClient } from '../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from './'
+
+export interface ICommand {
+    client?: WAClient
+    handler?: MessageHandler
+    run(M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void | never> | void | never
+    config: {
+        adminOnly?: boolean
+        aliases?: string[]
+        description?: string
+        command: string
+        id?: string
+        category?: TCategory
+        usage?: string
+        dm?: boolean
+        baseXp?: number
+        modsOnly?: boolean
+    }
+}
+
+export type TCategory =
+    | 'anime'
+    | 'bots'
+    | 'config'
+    | 'dev'
+    | 'fun'
+    | 'games'
+    | 'educative'
+    | 'general'
+    | 'media'
+    | 'moderation'
+    | 'category'
+    | 'reactions2'
+    | 'reactions'
+    | 'misc'
+    | 'utils'
+    | 'weeb'
+    | 'nsfw'
