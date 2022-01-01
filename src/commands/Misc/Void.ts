6e91430b3435148cef2277678152ba80d4f4c81b
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-01 20:13:28 +0530

    🕷

---
+++ src/commands/Misc/Void.ts
@@ -0,0 +1,31 @@
+import { MessageType } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IPackage, ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'ichigo',
+            description: 'Displays the info',
+            category: 'misc',
+            usage: `${client.config.prefix}void`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const pkg: IPackage = require(join(__dirname, '..', '..', '..', 'package.json'))
+        const image = this.client.assets.get('hmm')
+        if (!image) return void null
+        return void M.reply(
+            image,
+            MessageType.image,
+            undefined,
+            undefined,
+            `🖤 *Ichigo-botto* 🖤\n\n🍀 *Description: ${pkg.description}*\n\n🌐 *URL:* https://github.com/pratyush4932/Ichigo-botto`
+        )
+    }
+}
